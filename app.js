a3f9f8c2cef17ae98b231665a7ee69a77a98ed57
venmsn@ke <svinayak994@gmail.com>
2022-11-15 00:44:47 +0530

    added mvc approach

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const morgan = require("morgan");
 const express = require("express");
 const mongoose = require("mongoose");
-const Blog = require("./models/blogs");
 const { result } = require("lodash");
 const { urlencoded } = require("express");
 const { render } = require("express/lib/response");
+const blogRoutes=require('./routes/blogRoutes')
 
 //express app
 const app = express();
@@ -40,53 +40,7 @@ app.get("/about", (req, res) => {
 });
 
 //Blogs routes
-
-// blog routes
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.post('/blogs',(req,res)=>{
-    const blog=new Blog(req.body);
-    blog.save().then((result)=>{
-      res.redirect('/blogs');
-    }).catch((err)=>{
-       console.log(err);
-    })
-})
-
-app.get('/blogs/:id',(req,res)=>{
-  const id=req.params.id;
-  Blog.findById(id).then((result)=>{
-    res.render('details',{blog:result,title:'Blog Details'})
-  }).catch((err)=>{
-    console.log(err);
-  })
-})
-
-app.delete('/blogs/:id', (req, res) => {
-  const id = req.params.id;
-  
-  Blog.findByIdAndDelete(id)
-    .then(result => {
-      res.json({ redirect: '/blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
+app.use('/blogs',blogRoutes);
 
 // app.get("/blogs/create", (req, res) => {
 //   res.render("create", { title: "create" });
---
+++ routes/blogRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const blogController = require("../controllers/blogController");
+
+router.get("/create", blogController.blog_create_get);
+
+router.get("/", blogController.blog_index);
+
+router.post("/", blogController.blog_create_post);
+
+router.get("/:id", blogController.blog_details);
+
+router.delete("/:id", blogController.blog_delete);
+
+module.exports = router;
